5a426a62b1dc20096167211eb93f8ab857b6315e
Brady England <bradyeng22@gmail.com>
2022-03-04 19:07:02 -0700

    almost done with first page

--- brady/src/App.js
+++ brady/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/Logo.png";
+import rabbit from "./assets/Rabbit.png";
+import shield from "./assets/Shield.png";
+import talkie from "./assets/Talkie.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head">
+        <img src={logo} alt="logo" />
+        <button>Contact</button>
+      </div>
+      <div className="boxes">
+        <div className="box one">
+          <div className="image-container">
+            <img className="image-talkie" src={talkie} alt="talkie" />
+          </div>
+          <h2>Heading Two</h2>
+          <p>
+            animis animo aperiri cognoscerem consuevit dapibus docui fabulis
+            intellegat laudari metum.
+          </p>
+          <p2>Learn More</p2>
+        </div>
+        <div className="box two">
+          <div className="image-container">
+            <img className="image-rabbit" src={rabbit} alt="rabbit" />
+          </div>
+          <h2>Heading Two</h2>
+          <p>
+            animis animo aperiri cognoscerem consuevit dapibus docui fabulis
+            intellegat laudari metum.
+          </p>
+          <p2>Learn More</p2>
+        </div>
+        <div className="box three">
+          <div className="image-container">
+            <img className="image-shield" src={shield} alt="shield" />
+          </div>
+          <h2>Heading Two</h2>
+          <p>
+            animis animo aperiri cognoscerem consuevit dapibus docui fabulis
+            intellegat laudari metum.
+          </p>
+          <p2>Learn More</p2>
+        </div>
+      </div>
+      <div className="head-one"></div>
     </div>
   );
 }
