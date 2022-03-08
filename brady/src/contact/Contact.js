c0808f48cd5758ade749dd2d3098dff5b40b6ca0
Brady England <bradyeng22@gmail.com>
2022-03-07 17:38:25 -0700

    Done with Part 1

--- brady/src/contact/Contact.js
+++ brady/src/contact/Contact.js
@@ -7,26 +7,54 @@ function Contact() {
   return (
     <div className="Contact">
       <div className="contact-header-container">
-        <img src={logo} alt="logo" />
-        <Link className="homebtn" to="/">
-          Home
-        </Link>
+        <div className="logo-container">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="home-con">
+          <Link className="homebtn" to="/">
+            Home
+          </Link>
+        </div>
       </div>
       <div className="contact-container">
         <div className="heading-one-contact">
-          <h1 className="underline-two">Heading</h1>
-          <h1>&nbsp;One</h1>
-          <p3>
-            animis animo aperiri cognoscerem consuevit dapibus docui fabulis
-            intellegat laudari metum. animis animo aperiri cognoscerem consuevit
-            dapibus docui fabulis intellegat laudari metum.
-          </p3>
-          <p3>
-            animis animo aperiri cognoscerem consuevit dapibus docui fabulis
-            intellegat laudari metum.
-          </p3>
+          <div className="heading-words-contact">
+            <h1 className="underline-two">Heading</h1>
+            <h1>&nbsp;One</h1>
+          </div>
+          <div className="p3-contact">
+            <p3>
+              animis animo aperiri cognoscerem consuevit dapibus docui fabulis
+              intellegat laudari metum. animis animo aperiri cognoscerem
+              consuevit dapibus docui fabulis intellegat laudari metum.
+            </p3>
+            <p3>
+              animis animo aperiri cognoscerem consuevit dapibus docui fabulis
+              intellegat laudari metum.
+            </p3>
+          </div>
         </div>
-        <div className="heading-two"></div>
+        <div className="heading-two">
+          <h2 className="heading-two-contact">Heading Two</h2>
+          <div className="form-container">
+            <div className="inputs">
+              <input placeholder="First Name" type="text"></input>
+              <input placeholder="Last Name" type="text"></input>
+            </div>
+            <div className="inputs">
+              <input placeholder="Title" type="text"></input>
+              <input placeholder="Email" type="text"></input>
+            </div>
+            <textarea
+              className="message"
+              placeholder="Message"
+              type="text"
+            ></textarea>
+          </div>
+          <div className="submitbtn-container">
+            <button className="submitbtn">Submit</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- brady/src/home/Home.js
+++ brady/src/home/Home.js
@@ -4,8 +4,49 @@ import rabbit from "../assets/Rabbit.png";
 import shield from "../assets/Shield.png";
 import talkie from "../assets/Talkie.png";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 function Home() {
+  const [show, setShow] = useState(false);
+  const [objNew, setObjNew] = useState([]);
+
+  const checkClick = () => {
+    if (show === true) {
+      alert("Button has already been pressed");
+    }
+  };
+
+  const setClick = () => {
+    let objNew = [];
+
+    const obj1 = [
+      "Matt Johnson",
+      "Matt Johnson",
+      "Matt Johnson",
+      "Bart Paden",
+      "Ryan Doss",
+      "Jared Malcolm",
+    ];
+
+    const obj2 = [
+      "Matt Johnson",
+      "Bart Paden",
+      "Bart Paden",
+      "Jordan Heigle",
+      "Jordan Heigle",
+      "Tyler Viles",
+    ];
+
+    for (let i = 0; i < obj1.length; i++) {
+      if (objNew.indexOf(obj1[i]) === -1) objNew.push(obj1[i]);
+    }
+    for (let i = 0; i < obj2.length; i++) {
+      if (objNew.indexOf(obj2[i]) === -1) objNew.push(obj2[i]);
+    }
+
+    setObjNew(objNew);
+  };
+
   return (
     <div className="home">
       <div className="head">
@@ -58,11 +99,21 @@ function Home() {
           Remove the duplicates in 2 Javascript objects (found in readme), add
           the results into an array and output the list names in an unordered
           list below the paragraph when
-          <button className="link">this link</button>is clicked. If the
-          operation is completed already, notify the user that this has already
-          been done.
+          <button
+            onClick={function () {
+              setClick();
+              setShow(true);
+              checkClick();
+            }}
+            className="link"
+          >
+            this link
+          </button>
+          is clicked. If the operation is completed already, notify the user
+          that this has already been done.
         </p3>
       </div>
+      {show ? <ul>{objNew.join(", ")}</ul> : null}
     </div>
   );
 }
